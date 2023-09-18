437141df4c4f88186fdb0f22735ee618d60e490b
VenusakaVXT <voxuantuan33@gmail.com>
2023-09-18 20:45:57 +0700

    Connect MySQL with Sequelize

---
+++ src/config/connectDB.js
@@ -0,0 +1,18 @@
+import { Sequelize } from 'sequelize'
+import { user, password } from '../connect/connect'
+
+const sequelize = new Sequelize('role_management', user, password, {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+const connection = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+export default connection
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import configViewEngine from './config/viewEngine'
 import initWebRoutes from './routes/web'
 import bodyParser from 'body-parser'
+import connection from './config/connectDB'
 require('dotenv').config()
 
 const app = express()
@@ -16,6 +17,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // Initial web routes
 initWebRoutes(app)
+
+// Test connection
+connection()
 
 app.listen(PORT, () => {
     console.log(`Run project on url localhost:${PORT}`)
